b41878ff4fae07cb0c72a85dab5d5099383a88de
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-02-17 10:48:05 +0200

    Loop exercises 6 and 7.

---
+++ loops-t6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops_t6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double number = 10;
+            double sqrnumber = 0;
+
+            Console.WriteLine("Number   Sqrt");
+            for (int i = 1; i < number+1; i++)
+            {
+                sqrnumber = Math.Sqrt(i);
+
+                Console.WriteLine("  {0} \t{1}", i, Math.Round(sqrnumber, 2));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ loops-t7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops_t7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1 = 1;
+            int number2 = 1;
+            int answer = 0;
+
+            for (int x = 1; x < 10; x++) // calculcates first number 9 times
+            {
+
+                for (int i = 1; i < 10; i++) // calculcates second number 9 times
+                {
+                    answer = number1 * number2; // the actual calculation itself
+                    Console.WriteLine("{0} x {1} = {2}", number1, number2, answer);
+                    number2 = number2 + 1; // increase second number by one for next loop
+                                   
+                }
+
+                number1 = number1 + 1; // increase first number by one for next loop
+                number2 = 1; // reset for next loop
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
